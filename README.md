502ffac24e88c5f8c3948c17a4ec7d892fc27752
Ionestar <kmahadew21@gmail.com>
2021-05-06 21:49:13 +0200

    Reminder

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # GeorgeWashington
 
 Keeps track of the number of times a person uses #general to issue bot commands, and will very politely notify them
+
+## TODO add actual purpose
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import discord
 from discord.ext.commands import Bot
+import os
 
 dictionary = {}
+
 
 def readtomap():
     print("loaded")
@@ -13,10 +15,12 @@ def readtomap():
             num = line[pos+2:]
             dictionary[name] = int(num)
 
+
 def addtofile(d={}):
     with open("stats.txt", "w") as f:
         for i in d:
             f.write(i + "::" + str(d[i]) + "\n")
+
 
 def increaseuser(user):
     if user in dictionary.keys():
@@ -24,19 +28,24 @@ def increaseuser(user):
     else:
         dictionary[user] = 1
 
+
 bot = Bot(command_prefix='.')
 
 
-ordinal = lambda n: "%d%s" % (n,"tsnrhtdd"[(n//10%10!=1)*(n%10<4)*n%10::4])
+def ordinal(n): return "%d%s" % (
+    n, "tsnrhtdd"[(n//10 % 10 != 1)*(n % 10 < 4)*n % 10::4])
+
+
 readtomap()
 mess = "This is the {} time you've messaged the bot on the wrong fucking discord channel. You fucking donkey. The #bot channel exists for a fucking reason"
 
+
 @bot.event
 async def on_ready():
-    await bot.change_presence(activity = discord.Game('Nekopara'))
+    await bot.change_presence(activity=discord.Game('kys'))
     print(f'Bot connected as {bot.user}')
-    
-	
+
+
 @bot.event
 async def on_message(message: discord.Message):
     if message.author == bot.user:
@@ -52,5 +61,5 @@ async def on_message(message: discord.Message):
             addtofile(dictionary)
             await message.channel.send(mess.format(ordinal(dictionary[message.author.name])))
 
-TOKEN = os.getenv('TOKEN')		
+TOKEN = os.getenv('TOKEN')
 bot.run(TOKEN)
